5ba696f1af90cc677e7853bdb6f0bacacebed40d
Iskandar Kurbonov <iskandar.kurbonov@onlyoffice.com>
2023-12-16 21:25:25 +0300

    Delete key

--- install/OneClickInstall/install-Debian/install-preq.sh
+++ install/OneClickInstall/install-Debian/install-preq.sh
@@ -56,7 +56,6 @@ fi
 echo -e "Package: *\nPin: origin \"packages.microsoft.com\"\nPin-Priority: 1002" | tee /etc/apt/preferences.d/99microsoft-prod.pref
 dpkg -i packages-microsoft-prod.deb && rm packages-microsoft-prod.deb
 
-apt-key adv --keyserver pgp.mit.edu --recv-keys A8D3785C
 MYSQL_REPO_VERSION="$(curl https://repo.mysql.com | grep -oP 'mysql-apt-config_\K.*' | grep -o '^[^_]*' | sort --version-sort --field-separator=. | tail -n1)"
 MYSQL_PACKAGE_NAME="mysql-apt-config_${MYSQL_REPO_VERSION}_all.deb"
 if ! dpkg -l | grep -q "mysql-server"; then
